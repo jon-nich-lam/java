a08f501fb63bc1ecc01bf559e62ca7212e763f31
Jon Lam <jonnichlam@gmail.com>
2024-08-06 20:57:37 -0700

    Add first example of HelloWorld.java and HelloWorld.class.

---
+++ l1_introduction_to_java/hello_world/HelloWorld.java
@@ -0,0 +1,6 @@
+// A simple program that prints Hello, World on the terminal
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello,World!");
+    }
+}
